b9216baa484f63c30a432e75c1c6938e850935b7
Vinay Ethiraj <pazuzu@PazuzuBook.local>
2021-05-19 22:27:47 -0400

    updated grouping code cell6 and added motif analysis of correlation

--- scripts/pca_grouping_algo.py
+++ scripts/pca_grouping_algo.py
@@ -56,3 +56,52 @@ report.show_html('pcaReport.html')
 # step 6 helper (always run this step before generating new pcaReport.html)
 train_feat = copy.deepcopy(deep_copy_train)
 test_feat = copy.deepcopy(deep_copy_test)
+
+###############################################################################################
+##                                    IMPROVED STEP 6                                        ##
+###############################################################################################
+
+# cell 5
+#improved step 6
+deep_copy_train = copy.deepcopy(train_feat)
+deep_copy_test = copy.deepcopy(test_feat)
+
+pca_analysis_map = {}
+pca_analysis_map['pca1'] = ['spacy_24', 'spacy_10']
+pca_analysis_map['pca2'] = ['gunning_fog', 'lix', 'words_per_sent']
+pca_analysis_map['pca3'] = ['gunning_fog', 'lix', 'words_per_sent', 'spacy_24']
+pca_analysis_map['pca4'] = ['gunning_fog', 'lix', 'words_per_sent', 'spacy_24', 'spacy_10']
+pca_analysis_map['pca5'] = ['gunning_fog', 'lix', 'words_per_sent', 'spacy_24', 'spacy_10', 'kincaid']
+pca_analysis_map['pca6'] = ['spacy_9', 'spacy_20']
+pca_analysis_map['pca7'] = ['spacy_133', 'spacy_184']
+pca_analysis_map['pca8'] = ['spacy_139', 'spacy_147', 'spacy_133']
+pca_analysis_map['pca9'] = ['spacy_139', 'spacy_147', 'spacy_133', 'spacy_67']
+pca_analysis_map['pca10'] = ['spacy_139', 'spacy_147', 'spacy_133', 'spacy_67', 'spacy_156']
+pca_analysis_map['pca11'] = ['spacy_84', 'spacy_162']
+pca_analysis_map['pca12'] = ['spacy_84', 'spacy_162', 'spacy_155']
+pca_analysis_map['pca13'] = ['spacy_208', 'spacy_232']
+pca_analysis_map['pca14'] = ['spacy_208', 'spacy_232', 'spacy_262']
+pca_analysis_map['pca15'] = ['spacy_208', 'spacy_232', 'spacy_262', 'spacy_261']
+pca_analysis_map['pca16'] = ['spacy_200', 'spacy_263']
+pca_analysis_map['pca17'] = ['spacy_298', 'VB']
+pca_analysis_map['pca18'] = ['spacy_298', 'VB', 'WRB']
+pca_analysis_map['pca19'] = ['WDT', 'JJ']
+pca_analysis_map['pca20'] = ['spacy_4', 'pronoun']
+pca_analysis_map['pca21'] = ['exclaims', 'questions', 'pronoun']
+pca_analysis_map['pca22'] = ['spacy_7', 'spacy_27']
+pca_analysis_map['pca23'] = ['spacy_103', 'spacy_97']
+pca_analysis_map['pca24'] = ['spacy_103', 'spacy_97', 'spacy_159']
+pca_analysis_map['pca25'] = ['spacy_103', 'spacy_97', 'spacy_159', 'spacy_60']
+pca_analysis_map['pca26'] = ['spacy_86', 'spacy_105']
+pca_analysis_map['pca27'] = ['spacy_160', 'spacy_203']
+pca_analysis_map['pca28'] = ['spacy_294', 'spacy_196']
+
+for key in pca_analysis_map:
+    pca_res, train_feat = train_pca(train_feat, pca_analysis_map[key], key)
+    test_feat = apply_pca(pca_res, test_feat)
+    report = sv.compare(train_feat[[key+'_1', key+'_2', 'target']], test_feat[[key+'_1', key+'_2']], 'target')
+    file_name = "EDA_motif_pca_grouping_R1_"+key+".html"
+    report.show_html(file_name)
+    train_feat = copy.deepcopy(deep_copy_train)
+    test_feat = copy.deepcopy(deep_copy_test)
+
